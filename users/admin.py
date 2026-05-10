2fbfe165b20964039641e11973f3a6ff2d518783
Apil Karki <apilkarki22@gmail.com>
2026-05-10 20:42:48 +0545

    fix(auth): add first and last name fields to admin and forms file

--- users/admin.py
+++ users/admin.py
@@ -11,13 +11,14 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ("email", "is_staff", "is_active",)
     fieldsets = (
         (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
         ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
     )
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
             "fields": (
-                "email", "password1", "password2", "is_staff",
+                "email","first_name", "last_name", "password1", "password2", "is_staff",
                 "is_active", "groups", "user_permissions"
             )}
         ),
