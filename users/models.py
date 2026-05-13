ace0928419165265e41460a0c825ac1a9fc5d4df
Apil Karki <apilkarki22@gmail.com>
2026-05-13 18:18:52 +0545

    refactor: updated user manager to follow django convention

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ class CustomUserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
-    def create_user(self, email, first_name, last_name, password, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """
         Create and save a user with the given email and password.
         """
@@ -26,7 +26,7 @@ class CustomUserManager(BaseUserManager):
         user.save()
         return user
 
-    def create_superuser(self, email, first_name, last_name, password, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         """
         Create and save a SuperUser with the given email and password.
         """
