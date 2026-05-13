4bc78270ec80810d8b90ee99ae89b7ac06264046
Apil Karki <apilkarki22@gmail.com>
2026-05-13 18:22:40 +0545

    refactor: ensure proper routing for save with self._db

--- users/models.py
+++ users/models.py
@@ -23,7 +23,7 @@ class CustomUserManager(BaseUserManager):
             **extra_fields
         )
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
