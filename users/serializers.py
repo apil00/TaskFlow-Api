9d3ac930d86814dfe5aa40b73ec016d87f1a1e75
Apil Karki <apilkarki22@gmail.com>
2026-05-13 21:13:28 +0545

    feat(auth): user login

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth import authenticate
 
 class RegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=6)
@@ -19,3 +20,22 @@ class RegistrationSerializer(serializers.ModelSerializer):
             **validated_data
         )
         return user
+
+class LoginSerializer(serializers.serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        email = data.get('email')
+        password = data.get('password')
+
+        if not email and not password:
+            raise serializers.ValidationError("Email and Password are required.")
+
+        user = authenticate(email=email, password=password)
+
+        if not user:
+            raise serializers.ValidationError("Invalid Credentials.")
+
+        data['user']=user
+        return data
--- users/views.py
+++ users/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import RegistrationSerializer
+from rest_framework.authtoken.models import Token
+from .serializers import RegistrationSerializer, LoginSerializer
 
 class RegisterView(APIView):
     def post(self, request):
@@ -11,3 +12,16 @@ class RegisterView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+
+            token, create = Token.objects.get_or_create(user=user)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
