31d3174d49b0c4b1faaee016f4798f5f94f1557b
Apil Karki <apilkarki22@gmail.com>
2026-05-14 12:45:49 +0545

    made first_name, last_name field required during registration.

--- users/serializers.py
+++ users/serializers.py
@@ -4,6 +4,9 @@ from django.contrib.auth import authenticate
 
 class RegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=6)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+
     class Meta:
         model = CustomUser
         fields = ['email', 'first_name', 'last_name', 'password']
